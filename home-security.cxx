40205047d4de2dbda3d8c87f3589013707d7da54
Harm Coenen <harm@familiecoenen.nl>
2019-12-13 20:49:03 +0100

    Add .gitignore, and understand base code

--- home-security.cxx
+++ home-security.cxx
@@ -31,149 +31,142 @@ bool signal_recieved = false;
 
 void sig_handler(int signo)
 {
-	if( signo == SIGINT )
-	{
-		printf("received SIGINT\n");
-		signal_recieved = true;
-	}
+    if( signo == SIGINT )
+    {
+        printf("\nreceived SIGINT");
+        signal_recieved = true;
+    }
 }
 
 int usage()
 {
-	printf("usage: detectnet-camera [-h] [--network NETWORK] [--threshold THRESHOLD]\n");
-	printf("                        [--camera CAMERA] [--width WIDTH] [--height HEIGHT]\n\n");
-	printf("Locate objects in a live camera stream using an object detection DNN.\n\n");
-	printf("optional arguments:\n");
-	printf("  --help            show this help message and exit\n");
-	printf("  --network NETWORK pre-trained model to load (see below for options)\n");
-	printf("  --overlay OVERLAY detection overlay flags (e.g. --overlay=box,labels,conf)\n");
-	printf("                    valid combinations are:  'box', 'labels', 'conf', 'none'\n");
+    printf("usage: home-security [--help] [--network NETWORK] [--threshold=THRESHOLD]\n");
+    printf("                     [--camera CAMERA] [--width=WIDTH] [--height=HEIGHT]\n\n");
+    printf("Locate objects in a live camera stream using an object detection DNN.\n\n");
+    printf("Arguments:\n");
+    printf("  --help            show this help message and exit\n");
+    printf("  --network NETWORK pre-trained model to load (see below for options)\n");
+    printf("  --overlay OVERLAY detection overlay flags (e.g. --overlay=box,labels,conf)\n");
+    printf("                    valid combinations are:  'box', 'labels', 'conf', 'none'\n");
     printf("  --alpha ALPHA     overlay alpha blending value, range 0-255 (default: 120)\n");
-	printf("  --camera CAMERA   index of the MIPI CSI camera to use (e.g. CSI camera 0),\n");
-	printf("                    or for VL42 cameras the /dev/video device to use.\n");
+    printf("  --camera CAMERA   index of the MIPI CSI camera to use (e.g. CSI camera 0),\n");
+    printf("                    or for VL42 cameras the /dev/video device to use.\n");
     printf("                    by default, MIPI CSI camera 0 will be used.\n");
-	printf("  --width WIDTH     desired width of camera stream (default is 1280 pixels)\n");
-	printf("  --height HEIGHT   desired height of camera stream (default is 720 pixels)\n");
-	printf("  --threshold VALUE minimum threshold for detection (default is 0.5)\n\n");
+    printf("  --width=WIDTH     desired width of camera stream (default is 1280 pixels)\n");
+    printf("  --height=HEIGHT   desired height of camera stream (default is 720 pixels)\n");
+    printf("  --threshold=VALUE minimum threshold for detection (default is 0.5)\n\n");
 
-	printf("%s\n", detectNet::Usage());
+    printf("%s\n", detectNet::Usage());
 
-	return 0;
+    return 0;
 }
 
 int main( int argc, char** argv )
 {
-	/*
-	 * parse command line
-	 */
-	commandLine cmdLine(argc, argv);
+    /*
+     * parse command line
+     */
+    commandLine cmdLine(argc, argv);
 
-	if( cmdLine.GetFlag("help") )
-		return usage();
-
-
-	/*
-	 * attach signal handler
-	 */
-	if( signal(SIGINT, sig_handler) == SIG_ERR )
-		printf("\ncan't catch SIGINT\n");
+    if( cmdLine.GetFlag("help") )
+        return usage();
 
 
-	/*
-	 * create the camera device
-	 */
-	gstCamera* camera = gstCamera::Create(cmdLine.GetInt("width", gstCamera::DefaultWidth),
-								   cmdLine.GetInt("height", gstCamera::DefaultHeight),
-								   cmdLine.GetString("camera"));
-
-	if( !camera )
-	{
-		printf("\ndetectnet-camera:  failed to initialize camera device\n");
-		return 0;
-	}
-	
-	printf("\ndetectnet-camera:  successfully initialized camera device\n");
-	printf("    width:  %u\n", camera->GetWidth());
-	printf("   height:  %u\n", camera->GetHeight());
-	printf("    depth:  %u (bpp)\n\n", camera->GetPixelDepth());
-	
-
-	/*
-	 * create detection network
-	 */
-	detectNet* net = detectNet::Create(argc, argv);
-	
-	if( !net )
-	{
-		printf("detectnet-camera:   failed to load detectNet model\n");
-		return 0;
-	}
-
-	// parse overlay flags
-	const uint32_t overlayFlags = detectNet::OverlayFlagsFromStr(cmdLine.GetString("overlay", "box,labels,conf"));
-	
-
-	/*
-	 * create openGL window
-	 */
-	//glDisplay* display = glDisplay::Create();
-	//if( !display ) printf("detectnet-camera:  failed to create openGL display\n");
+    /*
+     * attach signal handler
+     */
+    if( signal(SIGINT, sig_handler) == SIG_ERR )
+        printf("\ncan't catch SIGINT");
 
 
-	/*
-	 * start streaming
-	 */
-	if( !camera->Open() )
-	{
-		printf("detectnet-camera:  failed to open camera for streaming\n");
-		return 0;
-	}
-	
-	printf("detectnet-camera:  camera open for streaming\n");
-	
-	
-	/*
-	 * processing loop
-	 */
-	float confidence = 0.0f;
-	
-	while( !signal_recieved )
-	{
-		// capture RGBA image
-		float* imgRGBA = NULL;
-		
-		if( !camera->CaptureRGBA(&imgRGBA, 1000) )
-			printf("detectnet-camera:  failed to capture RGBA image from camera\n");
+    /*
+     * create the camera device
+     */
+    gstCamera* camera = gstCamera::Create(cmdLine.GetInt("width", gstCamera::DefaultWidth),
+                                          cmdLine.GetInt("height", gstCamera::DefaultHeight),
+                                          cmdLine.GetString("camera"));
 
-		// detect objects in the frame
-		detectNet::Detection* detections = NULL;
-	
-		const int numDetections = net->Detect(imgRGBA, camera->GetWidth(), camera->GetHeight(), &detections, overlayFlags);
-		
-		if( numDetections > 0 )
-		{
-			printf("%i objects detected\n", numDetections);
-		
-			for( int n=0; n < numDetections; n++ )
-			{
-				printf("detected obj %i  class #%u (%s)  confidence=%f\n", n, detections[n].ClassID, net->GetClassDesc(detections[n].ClassID), detections[n].Confidence);
-				printf("bounding box %i  (%f, %f)  (%f, %f)  w=%f  h=%f\n", n, detections[n].Left, detections[n].Top, detections[n].Right, detections[n].Bottom, detections[n].Width(), detections[n].Height()); 
-			}
-		}	
+    if( !camera )
+    {
+        printf("\nhome-security: failed to initialize camera device\n");
+        return 0;
+    }
+    
+    printf("\nhome-security: successfully initialized camera device");
+    printf("\n    width:  %u", camera->GetWidth());
+    printf("\n   height:  %u", camera->GetHeight());
+    printf("\n    depth:  %u (bpp)\n", camera->GetPixelDepth());
+    
 
-	}
+    /*
+     * create detection network
+     */
+    detectNet* net = detectNet::Create(argc, argv);
+    
+    if( !net )
+    {
+        printf("\nhome-security: failed to load detectNet model\n");
+        return 0;
+    }
 
-	/*
-	 * destroy resources
-	 */
-	printf("detectnet-camera:  shutting down...\n");
-	
-	SAFE_DELETE(camera);
-	//SAFE_DELETE(display);
-	SAFE_DELETE(net);
+    printf("\nhome-security:  threshold is [%f]", net->GetThreshold());
+    printf("\nhome-security:  max detections is [%i]", net->GetMaxDetections());
 
-	printf("detectnet-camera:  shutdown complete.\n");
-	return 0;
+
+    // parse overlay flags
+    const uint32_t overlayFlags = detectNet::OverlayFlagsFromStr(cmdLine.GetString("overlay", "box,labels,conf"));
+    
+
+    /*
+     * start streaming
+     */
+    if( !camera->Open() )
+    {
+        printf("\nhome-security:  failed to open camera for streaming\n");
+        return 0;
+    }
+    
+    printf("\nhome-security:  camera open for streaming");
+    
+    
+    /*
+     * processing loop
+     */
+    while( !signal_recieved )
+    {
+        // capture RGBA image
+        float* imgRGBA = NULL;
+        
+        if( !camera->CaptureRGBA(&imgRGBA, 1000) )
+            printf("\nhome-security:  failed to capture RGBA image from camera");
+
+        // detect objects in the frame
+        detectNet::Detection* detections = NULL;
+    
+        const int numDetections = net->Detect(imgRGBA, camera->GetWidth(), camera->GetHeight(), &detections, overlayFlags);
+        
+        if( numDetections > 0 )
+        {
+            printf("\n%i objects detected", numDetections);
+        
+            for( int n=0; n < numDetections; n++ )
+            {
+                printf("\ndetected obj %i  class #%u (%s)  confidence=%f", n, detections[n].ClassID, net->GetClassDesc(detections[n].ClassID), detections[n].Confidence);
+                printf("\nbounding box %i  (%f, %f)  (%f, %f)  w=%f  h=%f", n, detections[n].Left, detections[n].Top, detections[n].Right, detections[n].Bottom, detections[n].Width(), detections[n].Height()); 
+            }
+        }   
+
+    }
+
+    /*
+     * destroy resources
+     */
+    printf("\nhome-security:  shutting down...");
+    
+    SAFE_DELETE(camera);
+    //SAFE_DELETE(display);
+    SAFE_DELETE(net);
+
+    printf("\nhome-security:  shutdown complete.\n");
+    return 0;
 }
-
-
