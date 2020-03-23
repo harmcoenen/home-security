929b555263e5b362306e8d57cce0a9cf89e891cb
Harm Coenen <harm@familiecoenen.nl>
2020-03-23 13:00:01 +0100

    Using USB camera and lower threshold value

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 export HS_USER="usernaam"
 export HS_PASS="paswoord"
 export IP_ADDR=`ifconfig eth0 | grep "inet " | cut -d ' ' -f 10`
 
-./home-security --ipaddr="$IP_ADDR" --user="$HS_USER" --password="$HS_PASS" --threshold=0.95 --overlay none
+./home-security --ipaddr="$IP_ADDR" --user="$HS_USER" --password="$HS_PASS" --camera=/dev/video1 --threshold=0.65 --overlay none
